24a0d7adfd6afda2766536221029608356c1d79a
kcgithubacct <thp800@hotmail.com>
2013-03-23 14:53:22 +0700

    Update a01.php

--- a01.php
+++ a01.php
@@ -25,8 +25,12 @@
 </head>
 <body>
 <h3>This page is create using text editor on github site  name is kcgithubacct</h3>
-<p>It works! You have to go to your github account under master directory add new or edit web page , Once you commit then you can view your new page right away at  cu-test.azurewebsites.net/a01.html  This is this page and just example</p>
-<?phpecho ("Hello World! This one is an echo by php");?>
+<p>It works! You have to go to your github account under master dir. add new or edit web page , Once you commit then you can view your new page right away at  cu-test.azurewebsites.net/a01.html  This is this page and just example</p>
+
+<?php
+echo ("Hello World! This one is an echo by php");
+?>
+
 <p>I will add some formatting now</p>
 <div>Hello. This is a DIV element.</div>
 <div id="div2">Hello. This is a DIV element.</div>
