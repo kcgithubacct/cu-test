f6794f58bde77a3492439947a8662371512adb73
kcgithubacct <thp800@hotmail.com>
2013-03-23 14:51:13 +0700

    Create a01.php

---
+++ a01.php
@@ -0,0 +1,34 @@
+<html>
+<head>    
+<style>
+   div  
+   {  
+   border:2px solid #a1a1a1;  
+   padding:10px 40px;
+   background:#dddddd;
+   width:300px;
+   border-radius:25px;
+   -moz-border-radius:25px; /* Old Firefox */
+   width:100px;
+   height:75px;
+   background-color:#dddddd;
+   }
+   div#div2
+   {  
+   transform:rotate(30deg);
+   -ms-transform:rotate(30deg); /* IE 9 */  
+   -moz-transform:rotate(30deg); /* Firefox */  
+   -webkit-transform:rotate(30deg); /* Safari and Chrome */  
+   -o-transform:rotate(30deg); /* Opera */
+   }
+   </style>
+</head>
+<body>
+<h3>This page is create using text editor on github site  name is kcgithubacct</h3>
+<p>It works! You have to go to your github account under master directory add new or edit web page , Once you commit then you can view your new page right away at  cu-test.azurewebsites.net/a01.html  This is this page and just example</p>
+<?phpecho ("Hello World! This one is an echo by php");?>
+<p>I will add some formatting now</p>
+<div>Hello. This is a DIV element.</div>
+<div id="div2">Hello. This is a DIV element.</div>
+</body>
+</html>
